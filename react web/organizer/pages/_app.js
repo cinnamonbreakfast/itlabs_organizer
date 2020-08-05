3c344d78e5cdc1e13532f2c3ee2d94b0de55595d
Andrew C <candetandrei@gmail.com>
2020-08-05 16:59:35 +0300

    Navi

--- react web/organizer/pages/_app.js
+++ react web/organizer/pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
+import TopNavigation from '../components/TopNavigation'
+
+const links = [
+  {
+    'url': '/',
+    'name': 'Home'
+  },
+  {
+    'url': '/about',
+    'name': 'About'
+  }
+]
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(useRouter().route)
+
+  return (
+    <div style={{height: '100vh'}}>
+      <TopNavigation links={links}/>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
