9b8d7a2cbff705db3b60102cb3be10c3035724c7
Oprisanu Odrin <odrinoprisanu@gmail.com>
2020-08-05 18:48:19 +0300

    created entity, repo,service,controller and dto for Company

---
+++ server/src/main/java/com/organizer/core/model/Company.java
@@ -0,0 +1,45 @@
+package com.organizer.core.model;
+
+
+import lombok.*;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import org.hibernate.annotations.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name="Company",schema="production")
+@javax.persistence.Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Builder
+@NamedQuery(name="Company.findByName", query = "select c from Company c where c.name = ?1")
+@NamedQuery(name="Company.findByCity", query = "select c from Company c where c.city = ?1")
+@NamedQuery(name="Company.findByCityAndCategory",query = "select c from Company c where c.city = ?1 and c.category = ?2")
+
+
+
+public class Company extends Entity<Long>{
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private String city;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private String name;
+
+    @Basic
+    @Column
+    private String category;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private String address;
+
+
+
+}
