c1ab3f13631341e53b59b93791d95383241853fb
Andrew C <candetandrei@gmail.com>
2020-08-13 20:48:37 +0300

    Search box server link first ver

--- react web/organizer/pages/index.js
+++ react web/organizer/pages/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/pages/home.module.scss'
 
+import axios from 'axios'
+
 export default function Home() {
   const [cityAutoSuggestion, toggleCitySuggestions] = useState(false);
   const [companyAutoSuggestion, toggleCompanySuggestions] = useState(false);
@@ -8,6 +10,8 @@ export default function Home() {
   const[criteria, setCriteria] = useState('company')
   const[location, setLocation] = useState('')
   const[searchString, setSearchString] = useState('')
+  
+  const[searchResult, setSearchResult] = useState(null)
 
   const handleLocation = (event) => {
     event.target.innerText && setLocation(event.target.innerText)
@@ -17,6 +21,59 @@ export default function Home() {
     toggleCitySuggestions(false)
     toggleCompanySuggestions(false)
   }
+
+  const handleFirstBox = (event) => {
+    setSearchString(event.target.value)
+
+
+    if(!event.target.value)
+    {
+      setSearchResult(null)
+    } else {
+      let data = new FormData();
+      data.set('type', 0)
+      data.set('search_input', searchString)
+      data.set('second_box', '')
+
+      axios.post(
+        'http://31.5.22.129:8080/map/sugestion',
+        data)
+        .then(resp => {
+          console.log(resp)
+          setSearchResult(resp.data)
+        }).catch(err => {
+          console.log(err)
+          return false;
+        })
+    }
+  }
+
+  const handleSecondBox = (event) => {
+    setLocation(event.target.value)
+
+    
+    if(!event.target.value)
+    {
+      setSearchResult(null)
+    } else {
+      let data = new FormData();
+      data.set('type', 0)
+      data.set('search_input', searchString)
+      data.set('second_box', '')
+
+      axios.post(
+        'http://31.5.22.129:8080/map/sugestion',
+        data)
+        .then(resp => {
+          setSearchResult(resp.data)
+        }).catch(err => {
+          console.log(err)
+          return false;
+        })
+    }
+  }
+
+  console.log(searchResult)
 
   return (
     <div className={styles.homeWrapper} onClick={(event) => bgOutClick(event)}>
@@ -32,38 +89,36 @@ export default function Home() {
               <option value="company">Company</option>
               <option value="service">Service</option>
             </select>
-            <input type="text" name="service" placeholder={`${criteria.charAt(0).toUpperCase()+criteria.slice(1)} name`} onClick={(e) => { e.stopPropagation(); bgOutClick(); toggleCompanySuggestions(true) }}/>
+            <input type="text" value={searchString} name="service" autoComplete="off" placeholder={`${criteria.charAt(0).toUpperCase()+criteria.slice(1)} name`} onClick={(e) => { e.stopPropagation(); bgOutClick(); toggleCompanySuggestions(true) }} onChange={(e) => handleFirstBox(e)}/>
             {
-              companyAutoSuggestion &&
+              Array.isArray(searchResult) && companyAutoSuggestion &&
               <div className={styles.autoSuggest + ' ' + styles.companies}>
                 <ul>
-                  {/* TODO: click pe companie */}
-                  <li>
-                    <h2>Company name</h2>
-                    <p>Cluj-Napoca, RO &bull; Tuns, Barbierit...</p>
+                  
+                  {
+                  Array.isArray(searchResult) && searchResult.reverse().map((each) => { return (<li>
+                    <h2>{each.company.name}</h2>
+                    <p>{`${each.company.city}, ${each.company.country}`} &bull; Tuns, Barbierit...</p>
                   </li>
+                  )}
+                  )}
 
-                  <li>
-                    <h2>Company name</h2>
-                    <p>Location &bull; Tuns, Barbierit, Pensat ...</p>
-                  </li>
-
-                  <li>
-                    <h2>Company name</h2>
-                    <p>Location &bull; Tuns, Barbierit, Pensat ...</p>
-                  </li>
                 </ul>
               </div>
             }
           </div>
 
           <div className={styles.formGroup} >
-            <input type="text" value={location} name="city" placeholder="City, country" autoComplete="off" onClick={(e) => { e.stopPropagation(); bgOutClick(); toggleCitySuggestions(true)}} onChange={(e) => setLocation(e.target.value)}/>
+            <input type="text" value={location} name="city" placeholder="City, country" autoComplete="off" onClick={(e) => { e.stopPropagation(); bgOutClick(); toggleCitySuggestions(true)}} onChange={(e) => handleSecondBox(e)}/>
             {
-              cityAutoSuggestion && 
+              Array.isArray(searchResult) && cityAutoSuggestion && 
               <div className={styles.autoSuggest}>
                 <ul>
-                  <li onClick={(e) => handleLocation(e)}>Cluj-Napoca, RO</li>
+                  {
+                    searchResult.reverse().map((each) => {
+                      return (<li onClick={(e) => handleLocation(e)}>{`${each.company.city}, ${each.company.country}`}</li>)
+                    })
+                  }
                 </ul>
               </div>
             }
