b6acc19cfae6cbb8a1886ea6c99e609d06798bbd
kreL-Lo <54546232+kreL-Lo@users.noreply.github.com>
2020-08-18 01:13:39 +0300

    new and improved search

--- server/src/main/java/com/organizer/core/repository/CompanyRepository.java
+++ server/src/main/java/com/organizer/core/repository/CompanyRepository.java
@@ -15,11 +15,18 @@ public interface CompanyRepository extends Repository<Long, Company>  {
     List<Company> findByCity(String city);
     Page<Company> findAll(Example e, Pageable page);
 
-    @Query(value ="select b from Company b where b.city like concat('%',:city,'%') and b.country like concat('%',:country,'%') and b.name like concat('%',:company,'%') ")
+    @Query(value ="select b from Company b " +
+            "where lower(b.city) like concat('%',lower(?1),'%')" +
+            " and lower(b.country) like concat('%',lower(?2),'%') " +
+            "and lower(b.name) like concat('%',lower(?3),'%') ")
     Page<Company>findByCountryAndCity(Pageable pageable, @Param("city")String city ,@Param("country") String country,@Param("company") String company);
 
 
-    @Query(value = "select distinct c from Company c join Specialist s on c=s.company join SpecialistService sp on sp.specialist=s where sp.serviceName like concat('%',?1,'%') and c.country like concat('%',?2,'%') and c.city like concat('%',?3,'%')" )
+    //@Query(value = "select distinct c from Company c join Specialist s on c=s.company join SpecialistService sp on sp.specialist=s where sp.serviceName like concat('%',?1,'%') and c.country like concat('%',?2,'%') and c.city like concat('%',?3,'%')" )\
+    @Query(value = "select distinct c from Company c join Specialist s on c=s.company join SpecialistService sp on sp.specialist=s where" +
+            " lower(sp.serviceName) like concat('%',lower(?1) ,'%') " +
+            "and lower(c.country) like concat('%',lower (?2),'%') " +
+            "and lower (c.city) like concat('%',lower(?3),'%')")
     Page<Company>findByService(Pageable pageable,@Param("service_name")String service_name ,@Param("county") String country , @Param("city") String city);
 
 }
--- server/src/main/java/com/organizer/core/service/CompanyService.java
+++ server/src/main/java/com/organizer/core/service/CompanyService.java
@@ -36,21 +36,7 @@ public class CompanyService {
 
     public List<Company> findByCountryAndCity(String city, String country,String company,int pageNumber,int pageSize){
         Pageable page = PageRequest.of(pageNumber,pageSize);
-        if(company.length()==0){
-            return this.companyRepository.findByCountryAndCity(page,city,country,company).getContent();
-        }
-        Page<Company> firstCheck =this.companyRepository.findByCountryAndCity(page,city,country,company);
-        if(firstCheck.getSize()>1){
-            return firstCheck.getContent();
-        }
-        StringConverter stringConverter = new StringConverter(company.toLowerCase());
-        Set<Company> companies = new LinkedHashSet<>(pageSize);
-        while(stringConverter.hasNext()&& companies.size()!=pageSize) {
-            String _company = stringConverter.getPermutation();
-            Page<Company> companyPage = this.companyRepository.findByService(page, _company, country, city);
-            companyPage.getContent().stream().forEach(e->{companies.add(e);});
-        }
-        return new ArrayList<>(companies);
+        return this.companyRepository.findByCountryAndCity(page,city,country,company).getContent();
     }
     public Company findById(Long id){
         return companyRepository.findById(id).get();
@@ -59,22 +45,7 @@ public class CompanyService {
     public List<Company> findByService(String serviceName,String country, String city,int pageNumber, int pageSize){
 
         Pageable pageable = PageRequest.of(pageNumber,pageSize);
-        if(serviceName.length()==0 ){
-            return this.companyRepository.findByService(pageable, serviceName, country, city).getContent();
-        }
-        Page<Company> firstCheck = this.companyRepository.findByService(pageable, serviceName, country, city);
-        if(firstCheck.getSize()>1){
-            return firstCheck.getContent();
-        }
-        StringConverter stringConverter = new StringConverter(serviceName.toLowerCase());
-        Set<Company> companies = new LinkedHashSet<>(pageSize);
-        while(stringConverter.hasNext()&& companies.size()!=pageSize) {
-            String service = stringConverter.getPermutation();
-            Page<Company> companyPage = this.companyRepository.findByService(pageable, service, country, city);
-            companyPage.getContent().stream().forEach(e->{companies.add(e);});
-        }
-        return new ArrayList<>(companies);
-
+        return this.companyRepository.findByService(pageable, serviceName, country, city).getContent();
     }
 
 
