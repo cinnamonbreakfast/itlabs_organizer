3477c8a3604954042c39a29bc433cf0c40cbb4cd
Andrew C <candetandrei@gmail.com>
2020-08-19 17:47:34 +0300

    Request attempt

--- server/src/main/java/com/organizer/web/controller/CompanyController.java
+++ server/src/main/java/com/organizer/web/controller/CompanyController.java
@@ -1,7 +1,9 @@
 package com.organizer.web.controller;
 
 import com.organizer.core.model.Company;
+import com.organizer.core.model.User;
 import com.organizer.core.service.CompanyService;
+import com.organizer.core.service.UserService;
 import com.organizer.web.auth.AuthStore;
 import com.organizer.web.dto.CompanyDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,32 +20,39 @@ public class CompanyController {
 
     private final CompanyService companyService;
     private final AuthStore authStore;
+    private final UserService userService;
 
     @Autowired
-    public CompanyController(CompanyService companyService, AuthStore authStore) {
+    public CompanyController(CompanyService companyService, AuthStore authStore, UserService userService) {
         this.companyService = companyService;
         this.authStore = authStore;
+        this.userService = userService;
     }
 
     @RequestMapping(value = "c/create",method = RequestMethod.POST)
-    public ResponseEntity<String> addNewCompany(String name,String city,String address, String category){
-        Company existingCompany = companyService.findByName(name);
+    public ResponseEntity<String> addNewCompany(@RequestBody CompanyDTO newCompany, @RequestHeader(name = "token") String token){
+        Company existingCompany = companyService.findByName(newCompany.getName());
+        Long userId = authStore.getUsername(token);
+        User owner;
+
+        username != null ? userService.findById(userId) : null;
 
         if(existingCompany == null){
             Company company = Company.builder()
-                    .name(name)
-                    .address(address)
-                    .category(category)
-                    .city(city)
+                    .name(newCompany.getName())
+                    .city(newCompany.getCity())
+                    .address(newCompany.getAddress())
+                    .category(newCompany.getCategory())
+                    .country(newCompany.getCountry())
                     .build();
 
             if(companyService.addNewCompany(company) != null)
-                return ResponseEntity.status(HttpStatus.OK).body("Reqistration complete.");
+                return ResponseEntity.status(HttpStatus.OK).body("Company was successfully added.");
 
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("There was a problem with your registration. Try again later.");
         }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("This name of the company is already used.");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("This company name is already used.");
     }
 
 
