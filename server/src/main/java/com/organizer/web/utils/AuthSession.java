4db73affbd6a70af5c3209eedd2e2b7d33e43d48
kreL-Lo <54546232+kreL-Lo@users.noreply.github.com>
2020-08-21 21:32:45 +0300

    smser

--- server/src/main/java/com/organizer/web/utils/AuthSession.java
+++ server/src/main/java/com/organizer/web/utils/AuthSession.java
@@ -1,8 +1,7 @@
 package com.organizer.web.utils;
 
 import lombok.*;
-import org.apache.tomcat.jni.Local;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
@@ -16,4 +15,16 @@ public class AuthSession {
     String token;
     LocalDateTime loginTime;
 
+    @Service
+    public static class SmsSender {
+
+        String url = "https://app.smso.ro/api/v1/";
+        String API_KEY ="mTr3xmoP3M9usuncicnqdD57DbxHlXWTpz4uePpz";
+        public void sendSms(String sender,String body){
+
+
+            //https://app.smso.ro/api/v1/send?sender=4&to=0722334455&body=Message%20Body&apiKey=mTr3xmoP3M9usuncicnqdD57DbxHlXWTpz4uePpz
+
+        }
+    }
 }
