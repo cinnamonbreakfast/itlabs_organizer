1e5a865fc843aa246ec6c1474ebdf628f427cca8
kreL-Lo <54546232+kreL-Lo@users.noreply.github.com>
2020-08-21 20:33:32 +0300

    f

--- server/src/main/java/com/organizer/web/controller/SmsController.java
+++ server/src/main/java/com/organizer/web/controller/SmsController.java
@@ -1,13 +1,26 @@
 package com.organizer.web.controller;
 
+import com.organizer.core.service.sms.SmsSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SmsController {
+    private final SmsSender smsSender;
 
-
-
-
+    @Autowired
+    public SmsController(SmsSender smsSender){
+        this.smsSender= smsSender;
+    }
+    @RequestMapping(value = "sms/send",method = RequestMethod.POST)
+    public ResponseEntity<String>  sendSms(@RequestParam String sender, @RequestParam String body){
+        smsSender.sendSms(sender,body);
+        return ResponseEntity.ok("done");
+    }
 
 
 }
